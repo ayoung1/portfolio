235aa353fd2973d0a00cb5fc018ed8208e56afbf
ayoung1 <aaron.young.0015@edmail.edcc.edu>
2014-07-09 16:52:52 -0700

    Most of the Content laid out and starting to style

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 $(document).foundation();
+
+$(document).foundation({
+  orbit: {
+      animation: 'fade', // Sets the type of animation used for transitioning between slides, can also be 'fade'
+      navigation_arrows: false,
+      slide_number: false,
+      bullets: false, // Does the slider have bullets visible?
+      timer: false, // Does the slider have a timer active? Setting to false disables the timer.
+      variable_height: true, // Does the slider have variable height content?
+  }
+});
