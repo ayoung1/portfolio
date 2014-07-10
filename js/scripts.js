c2b1610c043f6226b7a44e47b2e93282206754bf
ayoung1 <aaron.young.0015@edmail.edcc.edu>
2014-07-09 21:00:38 -0700

    jquery functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 $(document).foundation();
 
+$(function(){
+    $('#center').css({'height':(($(document).height())-162)+'px'});
+
+    $(window).resize(function(){
+    $('#center').css({'height':(($(document).height())-162)+'px'});
+    });
+});
+
 $(document).foundation({
   orbit: {
       animation: 'fade', // Sets the type of animation used for transitioning between slides, can also be 'fade'
@@ -10,3 +18,40 @@ $(document).foundation({
       variable_height: true, // Does the slider have variable height content?
   }
 });
+
+/*Menu Scroll*/
+var scrollspeed = 1000;
+
+$("#aboutb").click(function()
+{
+    scrollTo($("#about"), scrollspeed)
+});
+
+$("#resumeb").click(function()
+{
+	scrollTo($("#resume"), scrollspeed)
+});
+
+$("#galleryb").click(function()
+{
+	scrollTo($("#index"), scrollspeed)
+});
+
+/*Show Gallery*/
+$("#galleryb").click(function(){
+	$("#top").toggleClass("move");
+
+});
+
+/*Functions*/
+function scrollTo(selector, speed, easing, offset) {
+
+    if ($(selector)) {
+        offset = (offset) ? offset : 0;
+        speed = (speed) ? speed : 400;
+        easing = (easing) ? easing : "swing";
+        var ypos = $(selector).offset().top - offset;
+ 
+        $('html, body').stop().animate({ scrollTop: ypos }, speed, easing);
+    }
+}
